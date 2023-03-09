f0f565274203ce75cf782b1377c1cb2e7631a6a3
MariaIsabel-90 <m.isabel.barros@gmail.com>
2023-03-09 20:58:29 -0300

    Implementação da calculadora de média

--- js/media.js
+++ js/media.js
@@ -16,9 +16,35 @@ function calcularMedia() {
   const nota2 = parseFloat(document.getElementById("nota2").value);
   const nota3 = parseFloat(document.getElementById("nota3").value);
   const nota4 = parseFloat(document.getElementById("nota4").value);
-
+  
+  // a média
+  let notafinal = (nota1 + nota2 + nota3 + nota4)/4;
   let mensagem = "";
   // evite mexer no código acima!
+
+  if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)){
+    alert("Favor inserir todas as notas para calcularmos a média.")
+  }
+
+  if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0){
+    alert("Favor inserir notas de 0 a 10.")
+  } else  if (notafinal ===0){
+    mensagem = "Infelizmente você zerou a prova :(";
+  } else if (notafinal > 0 && notafinal<= 3) {
+    mensagem = "Caramba, deu ruim, você obteve média " + notafinal + "! " + " Estude mais e tente novamente!";
+  } else if (notafinal > 3 && notafinal <=5.9) {
+    mensagem = "Você obteve média " + notafinal + "! " + "Falta pouco para a média.";
+  } else if (notafinal > 5.9 && notafinal <= 7){
+    mensagem = "Você está na média com " + notafinal;
+  } else if (notafinal > 7 && notafinal <= 9.9) {
+    mensagem = "Notão! Sua média é " + notafinal + "!"
+  } else if (notafinal ===10) {
+    mensagem = "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média.";
+  }
+
+  if (notafinal >10){
+    alert("Insira valores de nota entre 0 e 10.")
+  }
 
 
 
